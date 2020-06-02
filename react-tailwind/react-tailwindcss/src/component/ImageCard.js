d0f8e3ced9b7fa42111aa6e2092d6179d5b319ea
Dimas Agusta Wiranata <dimaswiranata8@gmail.com>
2020-06-02 12:55:21 +0700

    finished galery pixabay

--- react-tailwind/react-tailwindcss/src/component/ImageCard.js
+++ react-tailwind/react-tailwindcss/src/component/ImageCard.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
 const ImageCard = ({image}) => {
+  const tags = image.tags.split(',');
+
   return(
     <div className="max-w-sm rounded overflow-hidden shadow-lg">
       <img src={image.webformatURL} alt="" className="w-full"/>
       <div className="px-6 py-4">
         <div className="font-bold text-purple-500 text-xl mb-2">
-          Photo by Jhon Doe
+          Photo by {image.user}
         </div>
         <ul>
           <li>
@@ -28,18 +30,11 @@ const ImageCard = ({image}) => {
         </ul>
       </div>
       <div className="px-6 py-4">
-        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
-          #tag1
-        </span>
-        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
-          #tag2
-        </span>
-        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
-          #tag3
-        </span>
-        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
-          #tag4
-        </span>
+        {tags.map((tag, index) => (
+          <span key={index} className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
+            #{tag}
+          </span>
+        ))}
       </div>
     </div>
   )
